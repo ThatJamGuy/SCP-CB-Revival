d60c530d99cf59a0b2791f43b7f9ada70a00d378
ThatJamGuy <soniccatcraft@gmail.com>
2024-11-07 18:59:33 -0500

    Blinking System / New Dev Level
    - Added the blinking system.
    - Works like the one from SCP:CB for the most part. Accurate timings will be configured later.
    - Infinite blink cheat.
    - Reworked the Dev Level to be less cluttered and easier to expand.
    - It will be featured in the first tech demo.

--- SCPCB Revival/Assets/Scripts/Player/PlayerController.cs
+++ SCPCB Revival/Assets/Scripts/Player/PlayerController.cs
@@ -21,6 +21,14 @@ public class PlayerController : MonoBehaviour
     [Header("Footstep Settings")]
     [SerializeField] private FootstepHandler footstepHandler;
 
+    [Header("Blinking Settings")]
+    public bool isBlinking = false;
+    [SerializeField] private Image blinkOverlay;
+    [SerializeField] private float blinkDrainRate = 0.2f;
+    [SerializeField] private Slider blinkSlider;
+    [SerializeField] private float blinkDepletionModifier = 0f;
+    [SerializeField] private bool infiniteBlink = false;
+
     [Header("Stamina Settings")]
     [SerializeField, Range(0, 1)] private float stamina = 1f;
     [SerializeField] private float maxStamina = 1f;
@@ -35,6 +43,10 @@ public class PlayerController : MonoBehaviour
     private float rotationX;
     private bool isMoving;
     private bool isSprinting;
+    private bool isBlinkingOverlayActive;
+    private float blinkTimer;
+    private float blinkOverlayDuration = 0.2f;
+    private float blinkOverlayHoldDuration = 0.2f;
 
     private void Start()
     {
@@ -42,6 +54,9 @@ public class PlayerController : MonoBehaviour
         footstepHandler.Initialize(characterController);
         UpdateCursorState();
         staminaSlider.value = stamina;
+        blinkTimer = 1f;
+        blinkSlider.value = blinkTimer;
+        blinkOverlay.enabled = false;
     }
 
     private void Update()
@@ -51,6 +66,7 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
         HandleLook();
         HandleStamina();
+        HandleBlinking();
         footstepHandler.UpdateFootsteps(isMoving, isSprinting);
     }
 
@@ -101,6 +117,54 @@ public class PlayerController : MonoBehaviour
         }
 
         staminaSlider.value = stamina / maxStamina;
+    }
+
+    private void HandleBlinking()
+    {
+        if (infiniteBlink)
+        {
+            blinkTimer = 1f;
+        }
+        else if (!isBlinkingOverlayActive)
+        {
+            float modifiedDrainRate = blinkDrainRate * (1 + blinkDepletionModifier);
+            blinkTimer = Mathf.Max(blinkTimer - modifiedDrainRate * Time.deltaTime, 0f);
+            if (blinkTimer == 0) TriggerBlink();
+        }
+
+        if (Input.GetKey(KeyCode.Space) && !infiniteBlink)
+        {
+            if (!isBlinkingOverlayActive)
+            {
+                blinkOverlay.enabled = true;
+                isBlinking = true;
+                isBlinkingOverlayActive = true;
+                blinkTimer = 1f;
+                CancelInvoke(nameof(EndBlink));
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) && isBlinkingOverlayActive)
+        {
+            Invoke(nameof(EndBlink), blinkOverlayHoldDuration);
+        }
+
+        blinkSlider.value = blinkTimer;
+    }
+
+    private void TriggerBlink()
+    {
+        blinkOverlay.enabled = true;
+        isBlinking = true;
+        isBlinkingOverlayActive = true;
+        blinkTimer = 1f;
+        Invoke(nameof(EndBlink), blinkOverlayDuration);
+    }
+
+    private void EndBlink()
+    {
+        blinkOverlay.enabled = false;
+        isBlinking = false;
+        isBlinkingOverlayActive = false;
     }
 
     private void UpdateCursorState()
