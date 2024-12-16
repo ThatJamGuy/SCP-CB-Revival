929ea137034f4a1e8c8d1002295c101130271b93
ThatJamGuy <soniccatcraft@gmail.com>
2024-12-16 15:43:14 -0500

    Main Menu settings
    - Added some Graphics and Audio option categories (Only the graphics settings work rn tho)
    - Added fullscreen toggle
    - Added vsync toggle
    
    And of course all made as closely as possible to the original.

--- SCPCB Revival/Assets/Scripts/Game Systems/MenuController.cs
+++ SCPCB Revival/Assets/Scripts/Game Systems/MenuController.cs
@@ -1,7 +1,16 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Main stuff for the Menu, but due to settings also being accessed from the pause menu I'll move all the settings
+/// stuff to another script later. Just not right now.
+/// </summary>
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private Toggle fullscreenToggle, vsyncToggle;
+    [SerializeField] private TMP_InputField frameLimitInputField;
+
     [SerializeField] private AudioClip menuMusic;
     [SerializeField] private AudioClip menuInteract;
     [SerializeField] private AudioClip menuFailInteract;
@@ -11,6 +20,13 @@ public class MenuController : MonoBehaviour
     private void Start()
     {
         MusicPlayer.Instance.StartMusic(menuMusic);
+
+        fullscreenToggle.isOn = Screen.fullScreen;
+
+        if(QualitySettings.vSyncCount == 0)
+            vsyncToggle.isOn = false;
+        else
+            vsyncToggle.isOn = true;
     }
 
     public void PlayInteractSFX(bool failed)
@@ -19,6 +35,21 @@ public class MenuController : MonoBehaviour
             interactSource.PlayOneShot(menuInteract);
         else
             interactSource.PlayOneShot(menuFailInteract);
+    }
+
+    public void ToggleFrameLimit()
+    {
+        frameLimitInputField.gameObject.SetActive(!vsyncToggle.isOn);
+    }
+
+    public void ApplyGraphics()
+    {
+        Screen.fullScreen = fullscreenToggle.isOn;
+
+        if(vsyncToggle.isOn)
+            QualitySettings.vSyncCount = 1;
+        else
+            QualitySettings.vSyncCount = 0;
     }
 
     public void OpenLink(string linkURL)
