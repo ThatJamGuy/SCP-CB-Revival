44565337382feb480355a92cbac13766fd71752c
ThatJamGuy <soniccatcraft@gmail.com>
2025-08-13 23:08:33 -0400

    [UNSTABLE] Fixed loading screen stuff
    - Fixed the issue relating to the old input system being removed. Also introduced a sort of artificial loading time, kind of.
    - (Possibly) fixed Ulgrin bad orientation post-breach

--- SCPCB Revival/Assets/Code/UI/LoadingScreenManager.cs
+++ SCPCB Revival/Assets/Code/UI/LoadingScreenManager.cs
@@ -1,45 +1,58 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 namespace scpcbr {
     public class LoadingScreenManager : MonoBehaviour {
-        [SerializeField] private GameObject[] loadingScreenInfo;
-        [SerializeField] private Slider progressBar;
-        [SerializeField] private TextMeshProUGUI progressText;
-        [SerializeField] private TextMeshProUGUI continuePromptText;
+        [SerializeField] GameObject[] loadingScreenInfo;
+        [SerializeField] Slider progressBar;
+        [SerializeField] TextMeshProUGUI progressText;
+        [SerializeField] TextMeshProUGUI continuePromptText;
+        [SerializeField] float minDisplayTime = 2f;
 
-        private bool canContinue = false;
+        bool canContinue;
+        float loadStartTime;
 
-        private void Start() {
+        void Start() {
+            //if (loadingScreenInfo.Length > 0)
             //loadingScreenInfo[Random.Range(0, loadingScreenInfo.Length)].SetActive(true);
+            // KEEPING THE THINGS OFF FOR NOW UNTIL I CAN GET SOMETHING BETTER
 
-            //MusicPlayer.Instance.ChangeMusic("Loading");
+            MusicPlayer.Instance.ChangeMusic("Loading");
 
-            if (continuePromptText != null)
-                continuePromptText.gameObject.SetActive(false);
+            if (continuePromptText) continuePromptText.gameObject.SetActive(false);
+            loadStartTime = Time.time;
         }
 
-        private void Update() {
-            if (SceneLoader.Instance.CurrentLoadOp != null) {
-                float normalizedProgress = Mathf.Clamp01(SceneLoader.Instance.CurrentLoadOp.progress / 0.9f);
-                progressBar.value = normalizedProgress;
-                progressText.text = $"Loading - {(normalizedProgress * 100):0}%";
+        void Update() {
+            var loader = SceneLoader.Instance;
+            if (loader.CurrentLoadOp != null) {
+                float progress = Mathf.Clamp01(loader.CurrentLoadOp.progress / 0.9f);
+                progressBar.value = progress;
+                progressText.text = $"Loading - {(progress * 100):0}%";
 
-                bool ready = SceneLoader.Instance.CurrentLoadOp.progress >= 0.9f &&
-                    SceneLoader.Instance.CurrentLoadType != LoadType.LoadMenu &&
-                    !SceneLoader.Instance.CanActivateScene;
+                bool ready = progress >= 1f &&
+                             loader.CurrentLoadType != LoadType.LoadMenu &&
+                             !loader.CanActivateScene &&
+                             Time.time - loadStartTime >= minDisplayTime;
 
-                if (continuePromptText != null)
-                    continuePromptText.gameObject.SetActive(ready);
+                if (continuePromptText) continuePromptText.gameObject.SetActive(ready);
                 canContinue = ready;
             }
-            if (canContinue && (Input.anyKeyDown || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))) {
+
+            if (canContinue && AnyInputPressed()) {
                 SceneLoader.Instance.ConfirmContinue();
                 canContinue = false;
-                if (continuePromptText != null)
-                    continuePromptText.gameObject.SetActive(false);
+                if (continuePromptText) continuePromptText.gameObject.SetActive(false);
             }
+        }
+
+        bool AnyInputPressed() {
+            return Keyboard.current.anyKey.wasPressedThisFrame ||
+                   Mouse.current.leftButton.wasPressedThisFrame ||
+                   Mouse.current.rightButton.wasPressedThisFrame ||
+                   Mouse.current.middleButton.wasPressedThisFrame;
         }
     }
 }
