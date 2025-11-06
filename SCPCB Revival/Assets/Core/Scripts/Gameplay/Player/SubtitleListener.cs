ad4518663307a533508b372f44ef67c60aec3d51
ThatJamGuy <soniccatcraft@gmail.com>
2025-11-05 20:54:43 -0500

    [UNSTABLE] Moving things
    Yeah

--- SCPCB Revival/Assets/Core/Scripts/Gameplay/Player/SubtitleListener.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace scpcbr {
-    public class SubtitleListener : MonoBehaviour {
-        public bool registerAsPlayer = true;
-        void OnEnable() { if (registerAsPlayer) SubtitleEmitter.RegisterListener(transform); }
-        void OnDisable() { if (registerAsPlayer) SubtitleEmitter.UnregisterListener(transform); }
-    }
-}
